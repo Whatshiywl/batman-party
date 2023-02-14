e6872bd275cdec84a88a68e759828e97345e5cd8
Whatshiywl <whatshiywl@gmail.com>
2023-02-14 12:17:25 -0300

    Implement main button room logic

--- src/app/components/batmongus/rooms/button/button-room.component.ts
+++ src/app/components/batmongus/rooms/button/button-room.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { DocumentReference } from "@angular/fire/compat/firestore";
 import { Observable } from "rxjs";
 import { BatmongusRoomComponent } from "../room.component";
 import { BatmongusButtonRoomService, ButtonState } from "./button-room.service";
@@ -10,8 +11,11 @@ import { BatmongusButtonRoomService, ButtonState } from "./button-room.service";
     <div
     class="button-room-button"
     [class.button-room-button-pressed]="(button$ | async)?.pressed"
+    (mousedown)="press()"
     (touchstart)="press()"
+    (mouseup)="release()"
     (touchend)="release()">
+      {{ ref?.id || '' }}
     </div>
   </div>
   `,
@@ -24,10 +28,14 @@ import { BatmongusButtonRoomService, ButtonState } from "./button-room.service";
   height: 30vh;
 
   > .button-room-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 20vh;
     height: 20vh;
     border-radius: 50%;
     background-color: red;
+    font-size: 15vh;
 
     &.button-room-button-pressed {
       filter: brightness(50%);
@@ -37,30 +45,30 @@ import { BatmongusButtonRoomService, ButtonState } from "./button-room.service";
   `]
 })
 export class BatmongusButtonRoomComponent extends BatmongusRoomComponent implements OnInit {
-  protected button$: Observable<ButtonState| undefined>;
+  protected button$?: Observable<ButtonState | undefined>;
+  protected ref?: DocumentReference<ButtonState>;
+
 
   constructor(
     private buttonRoomService: BatmongusButtonRoomService
   ) {
     super();
-    this.button$ = this.buttonRoomService.getButton$(1);
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.setTimeout(10000);
+    this.ref = await this.buttonRoomService.claim(this.timeoutValue);
+    this.button$ = this.buttonRoomService.getButton$(this.ref.id);
   }
 
-  @HostListener('window:beforeunload', [ '$event' ])
-  async beforeUnloadHandler() {
-    await this.release();
+  protected press() {
+    if (!this.ref) return;
+    return this.buttonRoomService.pressButton(this.ref.id);
   }
 
-  press() {
-    return this.buttonRoomService.pressButton(1);
-  }
-
-  release() {
-    return this.buttonRoomService.releaseButton(1);
+  protected async release() {
+    if (!this.ref) return;
+    return this.buttonRoomService.releaseButton(this.ref.id);
   }
 
 }
--- src/app/components/batmongus/rooms/button/button-room.service.ts
+++ src/app/components/batmongus/rooms/button/button-room.service.ts
@@ -1,44 +1,54 @@
 import { Injectable } from "@angular/core";
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/compat/firestore";
 import { Puzzle } from "../../batmongus.service";
 
 export interface BatmongusButtonRoom extends Puzzle {
-  buttonStates: boolean[];
   maxButtons: number;
 }
 
 export interface ButtonState {
+  claimedAt: number;
   pressed: boolean;
 }
 
 @Injectable()
 export class BatmongusButtonRoomService {
-  private roomRef: AngularFirestoreDocument<BatmongusButtonRoom>;
-  private room$?: BatmongusButtonRoom;
   private buttonsCol: AngularFirestoreCollection<ButtonState>;
 
   constructor(
-    afs: AngularFirestore
+    private afs: AngularFirestore
   ) {
-    this.roomRef = afs.collection('puzzles/batmongus/rooms').doc('button');
-    this.roomRef.valueChanges();
-    this.buttonsCol = this.roomRef.collection('buttons');
+    const roomRef = this.afs.collection('puzzles/batmongus/rooms').doc<BatmongusButtonRoom>('button');
+    this.buttonsCol = roomRef.collection('buttons');
   }
 
-  pressButton(index: number) {
+  async claim(timeout: number) {
+    return this.afs.firestore.runTransaction(async transaction => {
+      const claimedAt = Date.now();
+      const timestamp = claimedAt - timeout;
+      const buttons = await this.buttonsCol.ref.where('claimedAt', '<', timestamp).get();
+      const doc = buttons.docs[Math.floor(Math.random() * buttons.size)];
+      const { ref } = doc;
+      await transaction.get(ref);
+      transaction.update(ref, { claimedAt, pressed: false });
+      return ref;
+    });
+  }
+
+  pressButton(index: string) {
     return this.setButtonState(index, true);
   }
 
-  releaseButton(index: number) {
+  releaseButton(index: string) {
     return this.setButtonState(index, false);
   }
 
-  private setButtonState(index: number, pressed: boolean) {
-    return this.buttonsCol.doc(index.toString()).update({ pressed });
+  private setButtonState(index: string, pressed: boolean) {
+    return this.buttonsCol.doc(index).update({ pressed });
   }
 
-  getButton$(index: number) {
-    return this.buttonsCol.doc(index.toString()).valueChanges();
+  getButton$(index: string) {
+    return this.buttonsCol.doc(index).valueChanges();
   }
 
 }
